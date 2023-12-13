029f5de86d9961beb997f184d78aeb8b4a93903d
USER <USER@DESKTOP-37TK09B>
2023-12-13 10:22:44 -0500

    examen

---
+++ src/ec/edu/intsuperior/vista/Aplicacion.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.intsuperior.vista;
+
+
+
+/**
+ *
+ * @author USER
+ */
+public class Aplicacion {
+    public static void main(String[] args) {
+        FrmFacebook facebook= new FrmFacebook();
+        facebook.setVisible(true);
+    }
+       
+}
